1a2369d0415a8b14ffbaaa3cea6ff219e421f291
Trevor Johnson <johnsontrevor55@gmail.com>
2024-06-11 15:33:23 -0700

    some refactoring

--- menu.py
+++ menu.py
@@ -57,42 +57,30 @@ place_order = True
 while place_order:
     print("From which menu would you like to order? ")
 
-    menu_keys_indexes = dict(enumerate(menu.keys(), start=1))
-    for key, value in menu_keys_indexes.items():
-        print(f"{key}: {value}")
+    for index, key in enumerate(menu.keys(), start=1):
+        print(f"{index}: {key}")
 
     menu_selection = input("Enter a number to select a menu category: ")
     if not menu_selection.isdigit() or not (1 <= int(menu_selection) <= len(menu)):
         print(f"Please enter a valid number. Received {menu_selection}.\n")
         continue
 
-    sub_menu = menu[menu_keys_indexes[int(menu_selection)]]
+    menu_selection = int(menu_selection)
+    sub_menu = menu[list(menu.keys())[menu_selection - 1]]
 
     print("Item # | Item name                | Price  ")
     print("-------|--------------------------|--------")
 
     i = 1
-    sub_menu_keys_indexes = {}
     for key, value in sub_menu.items():
-        if type(value) == dict:
+        if isinstance(value, dict):
             for key2, value2 in value.items():
                 name = f"{key} - {key2}"
-                sub_menu_keys_indexes[i] = {
-                    "Name": name,
-                    "Price": value2,
-                    "Quantity": 0,
-                }
                 print(f"{i:<7}| {name:<25}| ${value2}")
                 i += 1
-            continue
-
-        print(f"{i:<7}| {key:<25}| ${value}")
-        sub_menu_keys_indexes[i] = {
-            "Name": key,
-            "Price": value,
-            "Quantity": 0,
-        }
-        i += 1
+        else:
+            print(f"{i:<7}| {key:<25}| ${value}")
+            i += 1
 
     item_number = input(
         "Enter a number to select an item. An invalid number will default to 1: "
@@ -100,17 +88,14 @@ while place_order:
     item_number = int(item_number) if item_number.isdigit() else 1
     item_number = max(1, min(item_number, len(sub_menu)))
 
-    order = sub_menu_keys_indexes[item_number]
-    item_name = order["Name"]
+    order = list(sub_menu.items())[item_number - 1]
+    item_name = order[0]
+    price = order[1] if isinstance(order[1], float) else list(order[1].values())[0]
 
     quantity = input(f"How many items of {item_name} would you like? ")
-    if not quantity.isdigit():
-        quantity = 1
+    quantity = int(quantity) if quantity.isdigit() else 1
 
-    quantity = int(quantity)
-    order["Quantity"] = quantity
-
-    order_list.append(order)
+    order_list.append({"Name": item_name, "Price": price, "Quantity": quantity})
 
     while True:
         keep_ordering = input("Would you like to keep ordering? (Y)es or (N)o ").lower()
@@ -130,7 +115,10 @@ for order in order_list:
     item_name = order["Name"]
     price = order["Price"]
     quantity = order["Quantity"]
+
     prices.append(price * quantity)
-    print(f"{item_name:<25}| ${price:<7}| {quantity}")
+    price_spaces = 3 if price > 10 else 2
+    name_spaces = 25 - len(item_name)
+    print(f"{item_name}{' ' * name_spaces}| ${price}{' ' * price_spaces}| {quantity}")
 
 print(f"The total order costs ${sum(prices)}")
